4fb90ba30ef5f72a33f3c427b9d8e9075f050752
Simon Westlin Green <simon.green@live.se>
2024-02-27 18:17:18 +0100

    Contactinfo working ish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import ThreePaneLayout from '@/components/ThreePaneLayout/ThreePaneLayout';
 import styles from './page.module.scss';
 import NewsList from '@/components/NewsList/NewsList';
+import ContactCard from '@/components/ContactCard/ContactCard';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <ThreePaneLayout
-        left={<div></div>}
-        middle={<NewsList />}
-        right={<div></div>}
-      />
+      <ThreePaneLayout left={<div></div>} middle={<NewsList />} right={<div><ContactCard></ContactCard></div>} />
     </main>
   );
 }
